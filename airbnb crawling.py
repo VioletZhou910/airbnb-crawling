650e69087fe0427bea6eb9841411a1c3bf19ad5a
VioletZhou910 <53334631+VioletZhou910@users.noreply.github.com>
2019-07-27 15:41:10 +0800

    airbnb crawling_V2

--- airbnb crawling.py
+++ airbnb crawling.py
@@ -27,23 +27,34 @@ def getRoomIDList(html_form):
     print(room_id_list)
     return room_id_list
 
-def getRoomInfo(room_id_list,html):
-    for room_id in room_id_list:
-        try:
-            format_url="https://www.airbnb.cn/api/v2/pdp_listing_details/"+room_id+"?_format=for_rooms_show&key=d306zoyjsyarp7ifhu67rjxn52tv0t20&"
-            JSON=getHTMLText(format_url)
-            print(JSON)
-            dic_JSON=json.loads(JSON)
-            reviews=dic_JSON["pdp_listing_detail"]["sorted_reviews"]
-            for i in reviews:
-                print(i.get("comments"))
-            time.sleep(5)
-        except:
+def getRoomLocation(dic_JSON):
+    Lat=dic_JSON["pdp_listing_detail"]["lat"]
+    Lon=dic_JSON["pdp_listing_detail"]["lng"]
+    print(Lat)
+    print(Lon)
+
+
+
+
+
+def getRoomCommentsFirst(dic_JSON):
+    reviews=dic_JSON["pdp_listing_detail"]["sorted_reviews"]
+    for i in reviews:
+        comment=i.get("comments")
+        print(comment)
+
+def getRoomCommentsNext(room_id):
+    for i in range(30):
+        if (i == 0):
             continue
-    
-    
-
-
+        else:
+            format_url_next="https://www.airbnb.cn/api/v2/homes_pdp_reviews?currency=CNY&key=d306zoyjsyarp7ifhu67rjxn52tv0t20&locale=zh&listing_id="+room_id+"&_format=for_p3&limit=7&offset="+str(i*7)+"&order=language_country"
+            JSON=getHTMLText(format_url_next)
+            dic_JSON=json.loads(JSON)
+            reviews=dic_JSON["reviews"]
+            for i in reviews:
+                comment=i.get("comments")
+                print(comment)
 
 
 
@@ -51,11 +62,16 @@ def main():
     url="https://www.airbnb.cn/s/%E4%B8%8A%E6%B5%B7%E9%95%BF%E5%AF%BF%E8%B7%AF/homes?refinement_paths%5B%5D=%2Fhomes&toddlers=0&hotSpots=hotSpots&scenic=scenic&subwayLine=subwayLine&businessArea=businessArea&college=college&district=district&station=station&map_toggle=false&hospital=hospital&place_id=ChIJ9YF3tuZvsjUREy8Og6A-abI&poi_tab=businessArea&poi_group=0&s_tag=Qn2rKnGt"
     html=getHTMLText(url)
     room_id_list=getRoomIDList(html)
-    getRoomInfo(room_id_list,html)
-
-
+    for room_id in room_id_list:
+        try:
+            format_url="https://www.airbnb.cn/api/v2/pdp_listing_details/"+room_id+"?_format=for_rooms_show&key=d306zoyjsyarp7ifhu67rjxn52tv0t20&"
+            JSON=getHTMLText(format_url)
+            dic_JSON=json.loads(JSON)
+            getRoomLocation(dic_JSON)
+            getRoomCommentsFirst(dic_JSON)
+            getRoomCommentsNext(room_id)
+            time.sleep(5)
+        except:
+            continue
 
 main()
-
-
-
